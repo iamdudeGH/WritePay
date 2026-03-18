771f0bc0afac2c139675bfb0cef6135614bfc527
Monu <monu@angels-MacBook-Air.local>
2026-03-18 16:32:41 +0530

    fix: remove stale lockfile and test scripts for clean Vercel build

--- frontend/test_rest.js
+++
@@ -1,4 +0,0 @@
-const Address = "0x41c3da9bb76099d3d6c53b206b944e30caba8d52f978678d376543f9f1590832";
-fetch(`https://api.testnet.aptoslabs.com/v1/accounts/23cb6500412437bf2d4ce56dd3d4c4f3a74b0fa0d3a51fef0aadd1fb03edec6d/transactions`)
-.then(x => x.json())
-.then(data => console.log(data))
